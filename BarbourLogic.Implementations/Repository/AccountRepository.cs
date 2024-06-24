1b0baf2e45113f54cf4bba211ca47df3b6887628
JabranShaheen <JabranShaheen@hotmail.com>
2024-06-24 01:23:47 +0100

    Implemented AccountRepository for IAccountRepository interface.

--- BarbourLogic.Implementations/Repository/AccountRepository.cs
+++ BarbourLogic.Implementations/Repository/AccountRepository.cs
@@ -1,20 +1,30 @@
-﻿using BarbourLogic.Abstractions.Entities;
+﻿using System;
+using System.Collections.Generic;
+using BarbourLogic.Abstractions.Entities;
 using BarbourLogic.Abstractions.Repository;
 
 namespace BarbourLogic.Implementations
 {
     public class AccountRepository : IAccountRepository
     {
+        // Simulating in-memory storage for accounts
+        private readonly Dictionary<string, Account> _accounts;
+
+        public AccountRepository()
+        {
+            _accounts = new Dictionary<string, Account>();
+        }
+
         public void AddAccount(Account account)
         {
-            // TODO: Implement logic to add an account to a data store (e.g., database, in-memory collection).
-            throw new System.NotImplementedException();
+
+            _accounts.Add(account.Id, account);
         }
 
         public Account GetAccountById(string id)
         {
-            // TODO: Implement logic to retrieve an account by its ID from a data store.
-            throw new System.NotImplementedException();
+            _accounts.TryGetValue(id, out Account account);
+            return account;
         }
 
         public void UpdateAccount(Account account)
