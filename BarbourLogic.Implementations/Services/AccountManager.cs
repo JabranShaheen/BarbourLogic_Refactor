8a8c80b2012d407ae4ba90a8c6d6c8c8980deb34
JabranShaheen <JabranShaheen@hotmail.com>
2024-06-24 01:41:32 +0100

    Added failing unit test for AddAccount method in AccountManager. Implemented AddAccount method in AccountManager.

---
+++ BarbourLogic.Implementations/Services/AccountManager.cs
@@ -0,0 +1,42 @@
+﻿using BarbourLogic.Abstractions.Services;
+using BarbourLogic.Abstractions.Repository;
+using BarbourLogic.Abstractions.Entities;
+using BarbourLogic.Abstractions.Services.BarbourLogic.Abstractions.Services;
+
+namespace BarbourLogic.Application
+{
+    public class AccountManager : IAccountManager
+    {
+        private readonly IAccountRepository _accountRepository;
+
+        public AccountManager(IAccountRepository accountRepository)
+        {
+            _accountRepository = accountRepository;
+        }
+
+        public void AddAccount(string id, string name)
+        {
+            // Create an Account object and pass it to the repository for storage
+            var account = new Account { Id = id, Name = name, Balance = 0 };
+            _accountRepository.AddAccount(account);
+        }
+
+        public void DepositMoney(string id, double amount)
+        {
+            // Implementation to be added
+            throw new System.NotImplementedException();
+        }
+
+        public void WithdrawMoney(string id, double amount)
+        {
+            // Implementation to be added
+            throw new System.NotImplementedException();
+        }
+
+        public Account GetAccountDetails(string id)
+        {
+            // Implementation to be added
+            throw new System.NotImplementedException();
+        }
+    }
+}
---
+++ BarbourLogic.Test/Services/AccountManagerTests.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Moq;
+using BarbourLogic.Abstractions.Services;
+using BarbourLogic.Abstractions.Repository;
+using BarbourLogic.Application;
+using BarbourLogic.Abstractions.Services.BarbourLogic.Abstractions.Services;
+using BarbourLogic.Abstractions.Entities;
+
+namespace BarbourLogic.Tests
+{
+    [TestClass]
+    public class AccountManagerTests
+    {
+        [TestMethod]
+        public void AddAccount_Should_Add_Account_Successfully()
+        {
+            // Arrange
+            var mockRepository = new Mock<IAccountRepository>();
+            IAccountManager accountManager = new AccountManager(mockRepository.Object);
+
+            string accountId = "1";
+            string accountName = "John Doe";
+
+            // Act
+            accountManager.AddAccount(accountId, accountName);
+
+            // Assert
+            mockRepository.Verify(r => r.AddAccount(It.IsAny<Account>()), Times.Once);
+            mockRepository.Verify(r => r.AddAccount(It.Is<Account>(a => a.Id == accountId && a.Name == accountName)), Times.Once);
+        }
+    }
+}
